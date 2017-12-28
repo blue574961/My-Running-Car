048e83a003dd9dc3bfb630d433515605b61a1c49
Yang <blue574961@gmail.com>
2017-12-28 20:43:31 +0800

    Add PlayerController

---
+++ My Running Car.Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+    private Rigidbody _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
