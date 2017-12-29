ce4c771a7b9b09e6a1aef672208c6d815425eb1e
Yang <blue574961@gmail.com>
2017-12-29 16:07:01 +0800

    Test1

--- My Running Car.Unity/Assets/Scripts/PlayerController.cs
+++ My Running Car.Unity/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     private Rigidbody _rigidbody;
+
+    [SerializeField, Range(0f, 50f)]
+    private float speed = 10f;
+
+    public float _horizontal = .0f;
+    public float _vertical = .0f;
+
     private void Awake()
     {
         _rigidbody = this.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update ()
+    private void Update ()
     {
-		
-	}
+        float deltaTime = Time.deltaTime;
+
+        _horizontal = Input.GetAxis("Horizontal") * speed * deltaTime;
+        _vertical = Input.GetAxis("Vertical") * speed * deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 origin = Vector3.zero;
+
+        Quaternion hq = Quaternion.AngleAxis(-_horizontal, Vector3.up);
+        Quaternion vq = Quaternion.AngleAxis(_vertical, Vector3.right);
+
+        Quaternion q = hq * vq;
+
+        _rigidbody.MovePosition(q * (transform.position - origin) + origin);
+        _rigidbody.MoveRotation(transform.rotation * q);    
+    }
+   
 }
